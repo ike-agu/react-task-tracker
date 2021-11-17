5ca87a8e6313400445d7c31938d298fbf34df5e2
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-11-17 00:13:26 +0000

    created header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from './components/Header'
+
 function App() {
   return (
     <div className="container">
-     <h1>Hello from Ikenna</h1>    
-     <h2>hello</h2>
+     <Header />
     </div>
   );
 }
