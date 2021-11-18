06992a036eb4a28c3e9fe620ef32649b747009dc
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-11-18 00:04:38 +0000

    react icons installed

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -6,10 +6,11 @@ const Tasks = ({tasks}) => {
     return ( 
         <>
             {tasks.map((task)=> (
-            <h3 key = {task.id} >{task.text}</h3>
+            <Task key = {task.id} task={task} />
             ))}
         </>
     )
 }
 
 export default Tasks
+  
